3ae84d707a6bbb844ee077ae1204dc36869caf83
Cian Scannell <cianmscannell@gmail.com>
2024-09-02 19:09:07 +0200

    update python version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,46 +10,47 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
 
-import subprocess
-import sphinx_rtd_theme
-
+sys.path.insert(0, os.path.abspath("."))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'Medical Image Analysis (8DC00)'
-copyright = '2021, Daniel Krahulec'
-author = 'Daniel Krahulec'
-
-# The full version, including alpha/beta/rc tags
-release = 'v0.1'
+project = "8DC00 - Medical Image Analysis"
+copyright = "2024 IMAG/e group"
+author = "Cian Scannell"
 
 
 # -- General configuration ---------------------------------------------------
+# -- General configuration
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
-    'nbsphinx',
-	'sphinx.ext.mathjax',
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
     "sphinx_rtd_theme",
-    'sphinx_gallery.load_style',
-    'sphinx_autobuild'
 ]
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+intersphinx_mapping = {
+    "rtd": ("https://docs.readthedocs.io/en/stable/", None),
+    "python": ("https://docs.python.org/3/", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
+}
+intersphinx_disabled_domains = ["std"]
+
+templates_path = ["_templates"]
+
+# -- Options for EPUB output
+epub_show_urls = "footnote"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
-
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -61,18 +62,6 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
-master_doc = 'index'
+html_static_path = ["_static"]
 
-highlight_language = 'python3'
-
-#nbsphinx_execute = 'never'
-nbsphinx_allow_errors = True
-
-nbsphinx_execute_arguments = [
-    "--InlineBackend.figure_formats={'svg', 'pdf'}",
-    "--InlineBackend.rc={'figure.dpi': 96}",
-]
-# pygments_style = 'sphinx'
-
-#subprocess.call(['sudo ../../publish.sh'])
+master_doc = "source/index"
